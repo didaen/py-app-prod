52c5955f07dffa2de757a82092b7e14dffc5c275
Dida En <firdanb@students.unnes.ac.id>
2022-05-21 15:19:20 +0700

    Menambahkan elseif untuk menampilkan pesan flash berhasil ganti password pada views>user>index.php.

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -10,6 +10,11 @@
                 <div class="w-full bg-green-100 text-green-600 border border-green-300 px-3 py-1 mb-4 rounded">
                     <?= $this->session->flashdata('ganti_profil'); ?>
                 </div>
+
+            <?php elseif ($this->session->flashdata('ganti_password')) : ?>
+                <div class="w-full bg-green-100 text-green-600 border border-green-300 px-3 py-1 mb-4 rounded">
+                    <?= $this->session->flashdata('ganti_password'); ?>
+                </div>
                 
             <?php endif; ?>
             <!-- AKHIR PESAN BERHASIL UBAH PROFIL -->
