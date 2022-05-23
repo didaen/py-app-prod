cd3316b2696e7635fde91aefc015de4710c2dbd0
Dida En <firdanb@students.unnes.ac.id>
2022-05-23 12:17:02 +0700

    Mengganti semua warna blue-1000 pada views>tentang menjadi blue-1400.

--- application/views/tentang/index.php
+++ application/views/tentang/index.php
@@ -1,6 +1,6 @@
 <div class="mb-10">
     <h1 class="bg-gray-300 text-lg font-bold text-blue-1300 text-center py-3">Pengembang</h1>
-    <div class="flex flex-col sm:flex-row items-center gap-4 px-4 sm:px-8 py-8 bg-white border-dashed border-l-8 border-r-8 border-blue-1000">
+    <div class="flex flex-col sm:flex-row items-center gap-4 px-4 sm:px-8 py-8 bg-white border-dashed border-l-8 border-r-8 border-blue-1400">
         <div>
             <img src="<?= base_url('assets/img/pylightgray.png'); ?>" alt="Pengembang" class="w-20">
         </div>
@@ -38,7 +38,7 @@
 <div class="mb-10">
 
     <h2 class="bg-gray-300 text-lg font-bold text-blue-1300 text-center py-3">Sekilas tentang pengembang</h2>
-    <div class="bg-white text-gray-600 px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1000">
+    <div class="bg-white text-gray-600 px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
         <p class="text-justify">
             Pengembang adalah mahasiswa akhir Universitas Negeri Semarang yang sedang mengerjakan skripsi. Web ini dibuat oleh pengembang karena terispirasi oleh buku Matematika Teknik oleh K.A Stroud dan web Personal Tutor oleh K.A. Stroud. Pengembang berharap dapat segera menyelesaikan skripsinya dan dapat menambah wawasan pengguna tentang fisika dan matematika.
         </p>
