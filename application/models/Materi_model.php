ca5bf400f0d3ed2a55b72e7eabf1e1dd7cda0220
Dida En <firdanb@students.unnes.ac.id>
2022-05-26 17:17:16 +0700

    Mengganti nama tabel dari card_content menjadi kunci_jawaban.

--- application/models/Materi_model.php
+++ application/models/Materi_model.php
@@ -149,7 +149,7 @@ class Materi_model extends CI_model
     public function getAnswerKey($materi_id, $sub_materi_id, $card)
     {
         $this->db->select('*');
-        $this->db->from('card_content');
+        $this->db->from('kunci_jawaban');
         $this->db->where('materi', $materi_id);
         $this->db->where('sub_materi', $sub_materi_id);
         $this->db->where('card', $card);
