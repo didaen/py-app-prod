f665c027cff71e435067cda8bfc4b55c1b62364e
Dida En <firdanb@students.unnes.ac.id>
2022-05-27 03:04:20 +0700

    Membuat method Materi_model::getMateriCard() yang digunakan untuk mengambil materi pada table card_materi di database.

--- application/models/Materi_model.php
+++ application/models/Materi_model.php
@@ -160,4 +160,22 @@ class Materi_model extends CI_model
             return false;
         }
     }
+
+
+
+    // Method untuk mengambil materi dalam card
+    public function getMateriCard($materi_id, $sub_materi_id, $card)
+    {
+        $this->db->select('*');
+        $this->db->from('card_materi');
+        $this->db->where('materi', $materi_id);
+        $this->db->where('sub_materi', $sub_materi_id);
+        $this->db->where('card', $card);
+        $query = $this->db->get();
+        if ($query->num_rows() != 0) {
+            return $query->result_array();
+        } else {
+            return false;
+        }
+    }
 }
