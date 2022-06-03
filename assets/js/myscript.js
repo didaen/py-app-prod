266dad62ddc99987aa5805f57a0c22e97795f253
Dida En <firdanb@students.unnes.ac.id>
2022-06-03 16:09:16 +0700

    Men-copy paste myscript.js yang ada di local ke production dan mengganti link-nya agar FETCHNYA dapat berjalan. Link ganti dari local ke physicsyourself.

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,7 +1,9 @@
-// Apabila pada database user sudah pernah menjawabnya, maka buat attribute read-only dan latar belakang warna ijo
+// FUNCTION UNTUK MENAMPILKAN MENAMPILKAN JAWABAN MENJADI READ-ONLY DAN MENGHITUNG PERSENTASE PENGERJAAN CARD
 function sudahDiJawab() {
+
 	// AMBIL SEMUA ELEMEN DENGAN CLASS pertanyaan
 	const semuaPertanyaan = document.querySelectorAll(".pertanyaan");
+
 	// MEMASUKKAN pertanyaan.length ke dalam variabel jumlahPertanyaan
 	let jumlahPertanyaan = semuaPertanyaan.length;
 
@@ -13,6 +15,7 @@ function sudahDiJawab() {
 
 		// JIKA SUDAH PERNAH DIJAWAB MAKA
 		if(pertanyaan.value !== '') {
+
 			// BGCOLORNYA BERUBAH MENJADI HIJAU
 			pertanyaan.style.backgroundColor = "#a3e635";
 
@@ -25,7 +28,6 @@ function sudahDiJawab() {
 			// persenPengerjaan BERTAMBAH
 			persenPengerjaan =
 				Math.round((1 - pertanyaanBelumDiisi / jumlahPertanyaan) * 100);
-			console.log(persenPengerjaan);
 
 			// WIDTH PROGRESS BAR AKAN BERTAMBAH
 			durasi.style.width = persenPengerjaan + "%";
@@ -38,45 +40,53 @@ function sudahDiJawab() {
 		}
 	});
 }
+
+// PEMANGGILAN FUNCTION
 sudahDiJawab();
+
 
 
 
 // MEMUNCULKAN NAVIGASI MENU SAAT KLIK IKON HAMBURGER
 function klikNav(checkboxNav) {
+
 	// JIKA CHECKBOX TERCENTANG
 	if (checkboxNav.checked) {
+
 		// AMBIL ELEMENT DENGAN ID menu-navigasi, LALU DISPLAY GANTI FLEX
 		document.getElementById("menu-navigasi").style.display = "flex";
 
 		// JIKA CHECKBOX TIDAK TERCENTANG
 	} else {
+
 		// AMBIL ELEMENT DENGAN ID menu-navigasi, LALU DISPLAYNYA DIKOSONGI
 		document.getElementById("menu-navigasi").style.display = "";
 	}
 }
 
 
+
+
 // FUNCTION CEK SCROLLBAR, TAMPIL PERINGATAN
 function hasScrollbar() {
+
+	// Mengambil semua element dengan class white-box dan memasukkannya ke dalam variabel whiteBox
 	const whiteBox = document.querySelectorAll(".white-box");
 	
+	// Mengambil semua elemen dengan class equation-box dan memasukkannya ke dalam constant equationBox
 	const equationBox = document.querySelectorAll(".equation-box");
-
 
 	// UNTUK MASING-MASING ELEMENT PADA HTMLCollection DENGAN CLASS whiteBox
 	for (let i = 0; i < whiteBox.length; i++) {
+
 		// DAN UNTUK MASING-MASING ELEMENT PADA HTMLCollection DENGAN CLASS equationBox
-		
-		if (
-			equationBox[i].clientHeight > whiteBox[i].clientHeight || whiteBox[i].scrollWidth > whiteBox[i].clientWidth
-		) {
+		if (equationBox[i].clientHeight > whiteBox[i].clientHeight || whiteBox[i].scrollWidth > whiteBox[i].clientWidth) {
 
 			// UBAH DISPLAY DARI ELEMENT DENGAN ID peringatan MANJADI GRID
 			document.getElementById("peringatan").style.display = "grid";
 			break;
 
-			// JIKA KONDISI TIDAK TERPENUHI
+		// JIKA KONDISI TIDAK TERPENUHI
 		} else {
 
 			// UBAH DISPLAY DARI ELEMENT DENGAN ID peringatan MANJADI GRID
@@ -88,11 +98,13 @@ function hasScrollbar() {
 
 
 
-// MENAMBAHKAN EVENT RESIZE KE WINDOW
+
+// MENAMBAHKAN EVENT LOAD KE WINDOW
 window.addEventListener("load", function () {
 	// APABILA WINDOW DIRESIZE, JALANKAN FUNCTION
 	hasScrollbar();
 });
+
 
 
 
@@ -104,23 +116,40 @@ window.addEventListener("resize", function () {
 
 
 
+
+// FUNCTION UNTUK MENGIRIMKAN DATA MATERI, SUB MATERI, DAN CARD NUMBER APABILA USER MELAKUKAN BACK TANPA MELALUI TOMBOL BACK YANG SUDAH DISEDIAKAN
 function kirimDataCard() {
-	let equationBox = document.querySelector('.isi-card');
+
+	// Pilih elemen dengan id isi-card, masukkan ke variabel equationBox
+	let equationBox = document.getElementById('isi-card');
+
+	// Tambahkan event click pada elemen tersebut
 	equationBox.addEventListener('click', function(event) {
+
+		// Masukkan elemen apa saja yang diklik oleh user yang berada di dalam elemen tersebut ke dalam variabel pertanyaanIni
 		let pertanyaanIni = event.target;
+
+		// Seleksi elemen yang memiliki id materi-id, kemudiam masukkan ke dalam variabel materiId
 		let materiId = document.getElementById('materi-id');
+
+		// Seleksi elemen yang memiliki id sub-materi-id, kemudiam masukkan ke dalam variabel subMateriId
 		let subMateriId = document.getElementById('sub-materi-id');
+
+		// Seleksi elemen yang memiliki id card-number, kemudiam masukkan ke dalam variabel cardNumber
 		let cardNumber = document.getElementById('card-number');
 		
+		// Jika variabel pertanyaanIni mempunyai class pertanyaan
 		if(pertanyaanIni.classList.contains('pertanyaan')) {
 			
-			// MENGIRIMKAN JAWABAN BENAR KE DATABASE
+			// Menginisiasi object baru FormData
 			let postData = new FormData();
+
+			// Mempersiapkan data-data yang akan dikirim menggunakan FETCH POST
 			postData.append('materi_id', materiId.value);
 			postData.append('sub_materi_id', subMateriId.value);
 			postData.append('card_number', cardNumber.value);
 			
-			
+			// FETCH POST UNTUK MENYIMPAN DATA KE TABEL user_learning_log untuk menyimpan data pembelajaran terakhir user
 			fetch('https://www.physicsyourself.com/materi/dataPembelajaranTerakhir', {
 				method: 'POST',
 				mode: 'no-cors',
@@ -170,56 +199,65 @@ satuKarakter();
 
 
 
+
 // FUNCTION UNTUK MELIHAT JAWABAN BENAR ATAU SALAH
 function tekanEnter() {
-	// AMBIL HTMLCOLLECTION DENGAN CLASS PERTANYAAN
+
+	// Seleksi semua elemen dengan class pertanyaan
 	const semuaPertanyaan = document.querySelectorAll(".pertanyaan");
 
-	// MENGAMBIL ELEMEN DENGAN id durasi
+	// Seleksi elemen dengan id durasi kemudian masukkan ke dalam variabel durasi
 	const durasi = document.querySelector("#durasi");
 
-	// MEMASUKKAN pertanyaan.length ke dalam variabel jumlahPertanyaan
+	// Menghitung panjang array semuaPertanyaan kemudian masukkan ke dalam variabel jumlahPertanyaan
 	let jumlahPertanyaan = semuaPertanyaan.length;
 
-	// NILAI AWAL DARI pertanyaanBelumDiisi sama dengan jumlahPertanyaan
+	// Nilai awal dari pertanyaanBelumDiisi sama dengan jumlahPertanyaan
 	let pertanyaanBelumDiisi = jumlahPertanyaan;
 	
+	// untuk masing-masing tiap elemen pada array semuaPertanyaan
 	semuaPertanyaan.forEach(pertanyaan => {
 
-		// JIKA SUDAH PERNAH DIJAWAB MAKA
+		// Jika kotak pertanyaan ada isinya
 		if(pertanyaan.value !== '') {
+
+			// Maka pertanyaanBelumDiisi jadi berkurang 1
 			pertanyaanBelumDiisi -= 1;
 		}
 	});
 
-	console.log(pertanyaanBelumDiisi);
-
 	// MENDEKLARASIKAN VARIABEL persenPengerjaan
 	let persenPengerjaan = 0;
-	console.log(persenPengerjaan);
 
 	// PADA TIAP-TIAP ELEMENT CLASS PERTANYAAN
 	semuaPertanyaan.forEach(pertanyaan => {
 
-		// SETIAP USER MEMASUKKAN JAWABAN
+		// Setiap user melakukan input jawaban
 		pertanyaan.addEventListener("keyup", function (event) {
 
-			// ARRAY-ARRAY YANG AKAN DIKIRMKAN MELALUI FORM DATA
+			// Membuat array kosong arrayIdPertanyaan untuk menyimpan kumpulan id elemen dengan class pertanyaan
 			let arrayIdPertanyaan = [];
+
+			// Membuat array kosong arrayJawabanUser untuk menyimpan kumpulan jawaban user pada input dengan class pertanyaan
 			let arrayJawabanUser = [];
+
+			// Pada tiap elemen class pertanyaan
 			semuaPertanyaan.forEach(pertanyaan => {
+
+				// Masukkan id ke dalam arrayIdPertanyaan
 				arrayIdPertanyaan.push(pertanyaan.id);
+
+				// Masukkan jawaban yang diinput user ke dalam arrayJawabanUser
 				arrayJawabanUser.push(pertanyaan.value);
 			});
-			console.log(arrayIdPertanyaan); 
-			console.log(arrayJawabanUser);
 
+			// Ketika user memasukkan jawaban pada input dengan class pertanyaan, ambil id-nya
+			let answ = pertanyaan.id;
+			
+			// Ambil value yang dimasukkan oleh user
+			let answValue = pertanyaan.value;
 
-			answ = pertanyaan.id;
-			console.log(answ);
-			answValue = pertanyaan.value;
-			console.log(answValue);
-
+			// FETCH POST jawaban untuk question box dengan id tersebut
 			fetch('https://www.physicsyourself.com/materi/answers', {
 				method: 'POST',
 				credentials: 'same-origin',
@@ -231,9 +269,10 @@ function tekanEnter() {
 			})
 				.then(response => response.json())
 				.then(response => {
-					console.log(response[answ]);
+					
 					// JIKA USER MENEKAN TOMBOL ENTER
 					if (event.keyCode === 13) {
+
 					// JIKA JAWABAN YANG DIINPUT OLEH USER SAMA DENGAN jawabanBenar
 						if (answValue == response[answ]) {
 
@@ -249,7 +288,6 @@ function tekanEnter() {
 							// persenPengerjaan BERTAMBAH
 							persenPengerjaan =
 								Math.round((1 - pertanyaanBelumDiisi / jumlahPertanyaan) * 100);
-							console.log(persenPengerjaan);
 
 							// WIDTH PROGRESS BAR AKAN BERTAMBAH
 							durasi.style.width = persenPengerjaan + "%";
@@ -277,6 +315,7 @@ function tekanEnter() {
 								console.log(res);
 							}).catch(console.log);
 						} else {
+
 							// APABILA JAWABAN SALAH BGCOLOR BERUBAH JADI MERAH
 							pertanyaan.style.backgroundColor = "#f87171";
 						}
@@ -294,22 +333,40 @@ tekanEnter();
 
 
 
+// Function untuk melakukan konfirmasi sebelum user melakukan logout
 function logout() {
+
+	// Buat variabel yang berisi semua elemen dengan class logout
 	let logoutButton = document.querySelectorAll(".logout");
+
+	// Untuk tiap elemen tersebut
 	logoutButton.forEach((lb) => {
+
+		// Ketika user melakukan klik
 		lb.addEventListener("click", function (event) {
+
+			// Muncul konfirmasi
 			logoutConfirm = confirm("Apakah Anda ingin logout?");
+
+			// Jika user memilih YES
 			if (logoutConfirm) {
+
+				// Kembalikan, atau keluar dari function
 				return lb;
 			}
+
+			// Menhindari tombol logout berjalan jika tidak diklik
 			event.preventDefault();
 		});
 	});
 }
+
+// Menjalankan function logout
 logout();
 
 
 
+// Funtion ini untuk melakukan penambahan attribut action pada saat memilih card atau menu dropdown card
 function klikGantiAction() {
 	let selectCard = document.getElementById("card");
 	let selectCardValue = selectCard.value;
@@ -320,6 +377,8 @@ function klikGantiAction() {
 
 
 
+// Funtion ini untuk menambahkan attribut action pada menu dropdown tiap-tiap card
+// Ini digunakan untuk navigasi card ketika sedang membuka card
 function klikGantiActionCardInternal() {
 	let selectCardInternal = document.getElementById("card-internal");
 	let selectCardInternalValue = selectCardInternal.value;
@@ -330,6 +389,7 @@ function klikGantiActionCardInternal() {
 
 
 
+// Function ini untuk menambahkan attribute action pada saat user klik tombol BACK
 function klikGantiActionBack() {
 	let cardNumberJudul = document.getElementById("card-number-judul");
 	let cardNumberJudulInnerHtml = parseInt(cardNumberJudul.innerHTML);
@@ -340,6 +400,7 @@ function klikGantiActionBack() {
 
 
 
+// Funtion ini untuk menyembunyikan tombol BACK saat user berada pada card 1
 function sembunyikanTombolBack() {
 	let tombolBack = document.getElementById("tombol-back");
 
@@ -354,20 +415,23 @@ function sembunyikanTombolBack() {
 		}
 	}
 }
+
+// Menjalankan function
 sembunyikanTombolBack();
 
 
 
+// Function ini untuk menambahkan attribute action pada saat user klik tombol NEXT
 function klikGantiActionNext() {
 	let cardNumberJudul = document.getElementById("card-number-judul");
 	let cardNumberJudulInnerHtml = parseInt(cardNumberJudul.innerHTML);
-	console.log(cardNumberJudulInnerHtml);
 	let formCard = document.getElementById("form-card-internal");
 	formCard.setAttribute("action", cardNumberJudulInnerHtml + 1);
 }
 
 
 
+// Function ini untuk menambahkan attribute action pada saat user klik tombol LANJUTKAN BELAJAR
 function klikGantiActionLanjutkanBelajar() {
 	let selectCard = document.getElementById("card-lanjutkan-belajar");
 	let selectCardValue = selectCard.value;
@@ -378,6 +442,7 @@ function klikGantiActionLanjutkanBelajar() {
 
 
 
+// Function ini untuk menyembunyikan tombol lanjutkan belajar apabila user masih baru atau masih belum ada record belajar terakhir
 function sembunyikanLanjutkanBelajarBtn() {
 	let lanjutkanBelajarBtn = document.getElementById("lanjutkan-belajar");
 
@@ -392,11 +457,13 @@ function sembunyikanLanjutkanBelajarBtn() {
 		}
 	}
 }
+
+// Menjalankan function
 sembunyikanLanjutkanBelajarBtn();
 
 
 
-// Tombol Reset untuk mengilangkan isi dari form
+// Tombol Reset untuk mengilangkan SEMUA JAWABAN pada card tersebut atau untuk mereset CARD
 function tombolReset() {
 
 	if(document.getElementById("reset") !== null) {
@@ -461,7 +528,6 @@ function showAnswers() {
 				.then(response => {
 
 					let klikTersimpan = parseInt(response['jumlah_klik']);
-					console.log(klikTersimpan);
 
 					let postData = new FormData();
 					postData.append("klik_tersimpan", klikTersimpan);
@@ -493,7 +559,6 @@ function showAnswers() {
 					})
 						.then(response => response.json())
 						.then(response => {
-							console.log(response);
 
 							response.forEach(response => {
 								kunciJawaban.innerHTML += response['isi'];
@@ -525,87 +590,6 @@ function showAnswers() {
 	}	
 }
 showAnswers();
-
-
-
-// FUNCTION UNTUK MENAMPILKAN MATERI
-function showMateri() {
-
-	if (document.getElementById("materi-pembelajaran") != null) {
-
-		let materiPembelajaran = document.getElementById("materi-pembelajaran");
-	
-		fetch('https://www.physicsyourself.com/materi/tampilkanMateriCard', {
-			method: 'POST',
-			credentials: 'same-origin',
-			mode: 'no-cors',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-		})
-			.then(response => response.json())
-			.then(response => {
-				console.log(response);
-				
-				let isiKolom = '';
-	
-				response.forEach(resp => {
-	
-					if(resp['layout'] == '1') {
-					
-						isiKolom += 
-							`
-								<div class="flex flex-col">
-									${resp['isi']}
-								</div>
-							`;
-		
-					} else {
-	
-						if(resp['kolom'] == '1') {
-	
-							isiKolom += 
-								`
-									<div class="flex flex-col sm:flex-row sm:gap-x-8">
-										${resp['isi']}
-								`;
-						} else {
-	
-							isiKolom += 
-								`
-										${resp['isi']}
-									</div>
-								`;
-						}
- 
-					}
-				});
-	
-				materiPembelajaran.innerHTML = isiKolom;
-										
-				renderMathInElement(
-					materiPembelajaran,
-					{
-						delimiters: [
-							{left: "$$", right: "$$", display: true},
-							{left: "$", right: "$", display: false},
-							{left: "\\begin{equation}", right: "\\end{equation}", display: true},
-							{left: "\\begin{align}", right: "\\end{align}", display: true},
-							{left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
-							{left: "\\begin{gather}", right: "\\end{gather}", display: true},
-							{left: "\\(", right: "\\)", display: false},
-							{left: "\\[", right: "\\]", display: true}
-						]
-					}
-				);
-				
-			});
-	}
-
-}
-
-showMateri();
 
 
 
