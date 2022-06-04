0adf701a813fca721d356ac5515c5c552c70a7c8
Dida En <firdanb@students.unnes.ac.id>
2022-06-04 12:50:19 +0700

    Menambahkan method construct dan memanggil method helper py is_logged_in() pada constroller Tentang.

--- application/controllers/Tentang.php
+++ application/controllers/Tentang.php
@@ -4,6 +4,18 @@
 // Membuat class Tentang yang menuruni semua method dan property dari class induknya (CI_Controller)
 class Tentang extends CI_Controller
 {
+
+    public function __construct()
+    {
+        // Memanggil method constructor dari class parent
+        // Parent-nya yait CI_Controller
+        parent::__construct();
+
+        is_logged_in();
+    }
+
+
+    
     public function index()
     {
         // Mengambil data user login dari database
