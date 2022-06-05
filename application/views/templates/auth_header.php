4a5fe8ff1ddbd0419e065ed6f8f53a407fd9ecc5
Dida En <firdanb@students.unnes.ac.id>
2022-06-06 06:24:48 +0700

    Menambahkan FAVICON <link> pada views > templates > auth_header.php juga.

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -6,6 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- FAVICON -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url(); ?>apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url(); ?>favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url(); ?>favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url(); ?>site.webmanifest">
+    <!-- AKHIR FAVICON -->
+
     <!-- TAILWIND -->
     <link rel="stylesheet" href="<?= base_url(); ?>assets/css/output.css">
     <!-- AKHIR TAILWIND -->
