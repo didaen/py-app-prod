d16d67fcdd6c2259df888671462662bbdff1c956
Dida En <firdanb@students.unnes.ac.id>
2022-06-06 04:14:20 +0700

    Menghapus FAVICON dari views > templates > header.php.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,15 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- FAVICON -->
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url()?>apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url()?>favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url()?>favicon-16x16.png">
-    <link rel="manifest" href="<?= base_url()?>site.webmanifest">
-    <meta name="msapplication-TileColor" content="#2b5797">
-    <meta name="theme-color" content="#ffffff">
-    <!-- AKHIR FAVICON -->
-
     <!-- TAILWIND -->
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/output.css">
     <!-- AKHIR TAILWIND -->
