9f80993b10e8471bcd51c6ddf497fde2c9674290
Dida En <firdanb@students.unnes.ac.id>
2022-06-06 11:37:25 +0700

    Merubah warna tombol jadi biru semua.

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -20,8 +20,8 @@
     <div class="p-4 flex flex-col sm:hidden space-y-8 bg-white border-dashed border-l-8 border-r-8 border-blue-1400">
         <p class="text-gray-600 text-justify">Physics Yourself adalah sebuah web untuk belajar Fisika dan Matematika. Web ini memiliki konsep belajar dimana saja dan kapan saja. Web ini masih dalam tahap pengembangan awal sehingga masih memerlukan banyak perbaikan. Bantu pengembang dengan mengisi angket berikut ini. Bantuan dari kamu sangat berarti.</p>
         <div class="flex flex-col justify-end space-y-2 sm:space-x-2">
-            <a href="<?= base_url('materi'); ?>" class="btn-kecil bg-indigo-600 hover:bg-indigo-500 text-gray-200">Belajar dulu</a>
-            <a href="https://www.youtube.com/channel/UC9hGq1nEAfPOvn4U41JLNSA" target=”_blank” class="btn-kecil bg-green-500 hover:bg-green-400 text-gray-200">Tutorial</a>
+            <a href="<?= base_url('materi'); ?>" class="btn-kecil">Belajar dulu</a>
+            <a href="https://www.youtube.com/channel/UC9hGq1nEAfPOvn4U41JLNSA" target=”_blank” class="btn-kecil">Tutorial</a>
             <a href="#" class="btn-kecil">Isi angket</a>
         </div>
     </div>
