5bc8f980358f03e1984b44084b7f2db342510008
Dida En <firdanb@students.unnes.ac.id>
2022-06-07 10:57:13 +0700

    Menambahkan jarak horizontal.

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -86,7 +86,7 @@
                                 <div class="white-box overscroll-auto overflow-auto text-sm mb-4">
                                     <div class="equation-box grid">
  
-                                        <div class="flex flex-row justify-between">
+                                        <div class="flex flex-row justify-between gap-x-8">
                                             <div>
 
                                                 <?= $pel['isi']; ?>
