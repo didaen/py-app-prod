c3e60d31a1b67085979d64471fd4a1177b1312d8
Dida En <firdanb@students.unnes.ac.id>
2022-06-08 13:52:51 +0700

    Menghapus pemanggilan pesan flash akun_baru.

--- application/views/auth/verification-forgot.php
+++ application/views/auth/verification-forgot.php
@@ -14,12 +14,7 @@
         <!-- AKHIR LOGO DAN JUDUL PHYSICS YOURSELF -->
 
         <!-- MENAMPILKAN FLASHDATA -->
-        <?php if ($this->session->flashdata('akun_baru')) : ?>
-            <div class="w-full bg-green-100 text-green-600 border border-green-300 px-3 py-1 mb-4 rounded">
-                <?= $this->session->flashdata('akun_baru'); ?>
-            </div>
-
-        <?php elseif ($this->session->flashdata('token_salah')) : ?>
+        <?php if ($this->session->flashdata('token_salah')) : ?>
             <div class="w-full bg-red-100 text-red-600 border border-red-300 px-3 py-1 mb-4 rounded">
                 <?= $this->session->flashdata('token_salah'); ?>
             </div>
