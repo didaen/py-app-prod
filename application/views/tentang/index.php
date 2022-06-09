6c3377e87c0e818aa08b12b3b84d6b76c9002c38
Dida En <firdanb@students.unnes.ac.id>
2022-06-09 13:51:19 +0700

    Mengatur peletakan foreach agar ordered list muncul.

--- application/views/tentang/index.php
+++ application/views/tentang/index.php
@@ -50,12 +50,12 @@
 
     <h2 class="bg-gray-300 text-lg font-bold text-blue-1300 text-center py-3">Referensi</h2>
     <div class="bg-white text-gray-600 px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
-        <?php foreach ($referensi as $ref) : ?>
         <ol class="block float-none">
-            <li>
-                <?= $ref['sumber']; ?>
-            </li>
+            <?php foreach ($referensi as $ref) : ?>
+                <li>
+                    <?= $ref['sumber']; ?>
+                </li>
+            <?php endforeach; ?>
         </ol>
-        <?php endforeach; ?>
     </div>
 </div>
