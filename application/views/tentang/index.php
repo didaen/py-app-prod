3c0a848ffd0661e468f2c9f5ace597d4c0861565
Dida En <firdanb@students.unnes.ac.id>
2022-06-09 13:49:42 +0700

    Menambhakan class block dan float-none agar <ol> bisa muncul.

--- application/views/tentang/index.php
+++ application/views/tentang/index.php
@@ -51,7 +51,7 @@
     <h2 class="bg-gray-300 text-lg font-bold text-blue-1300 text-center py-3">Referensi</h2>
     <div class="bg-white text-gray-600 px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
         <?php foreach ($referensi as $ref) : ?>
-        <ol>
+        <ol class="block float-none">
             <li>
                 <?= $ref['sumber']; ?>
             </li>
