4d11464b0740a87a845912f0c7b602e45a8a3943
Dida En <firdanb@students.unnes.ac.id>
2022-06-09 13:23:37 +0700

    Menambahkan data['judul'] untuk controller Tentang.

--- application/controllers/Tentang.php
+++ application/controllers/Tentang.php
@@ -24,13 +24,14 @@ class Tentang extends CI_Controller
         // Mengambil data user login dari database
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
+        // Memanggil database untuk meload referensi web ini
         $data['references'] = $this->Tentang_model->getAllReferences();
 
-        $this->load->view('templates/header', $data);
-
         // Pada controller Materi ini akan meload view tentang/index
-        $this->load->view('tentang/index');
+        $data['judul'] = 'Tentang';
 
+        $this->load->view('templates/header', $data);
+        $this->load->view('tentang/index', $data);
         $this->load->view('templates/footer');
     }
 }
