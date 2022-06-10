3d460972c8eb162a237125a2616c680ccbdaf99d
Dida En <firdanb@students.unnes.ac.id>
2022-06-10 11:43:52 +0700

    Mengganti classnya dengan flex.

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -85,10 +85,10 @@
                                     <div class="equation-box grid">
                                         <div class="flex items-center gap-x-4">
 
-                                            <div class="w-10/12 overflow-hidden">
+                                            <div class="flex w-10/12 overflow-hidden">
                                                 <?= $pel['isi']; ?>
                                             </div>
-                                            <div class="w-2/12 text-right overflow-hidden">
+                                            <div class="flex w-2/12 text-right overflow-hidden">
                                                 $(1)$
                                             </div>
                                         </div>
