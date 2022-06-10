0b6a7738b608a64fb34746fb1efb2c869b1429ce
Dida En <firdanb@students.unnes.ac.id>
2022-06-10 10:47:22 +0700

    items-center.

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -83,7 +83,7 @@
 
                                 <div class="white-box overscroll-auto overflow-auto w-full sm:w-<?= $pel['ukuran_kolom']?>">
                                     <div class="equation-box grid">
-                                        <div class="flex">
+                                        <div class="flex items-center">
 
                                             <div>
                                                 <?= $pel['isi']; ?>
