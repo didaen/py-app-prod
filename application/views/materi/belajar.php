be77784fbec987a88d43373b64bda19d7df5a007
Dida En <firdanb@students.unnes.ac.id>
2022-06-10 11:23:20 +0700

    Menambahkan class overflow-hidden.

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -83,7 +83,7 @@
 
                                 <div class="white-box overscroll-auto overflow-auto w-full sm:w-<?= $pel['ukuran_kolom']?>">
                                     <div class="equation-box grid">
-                                        <div class="flex items-center">
+                                        <div class="flex items-center overflow-hidden">
 
                                             <div class="w-10/12">
                                                 <?= $pel['isi']; ?>
