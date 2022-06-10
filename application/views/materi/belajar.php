5838d4d368c54269f9217287fd6e41a75f29edb0
Dida En <firdanb@students.unnes.ac.id>
2022-06-10 12:32:50 +0700

    Flex flex-row.

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -82,7 +82,7 @@
                             <?php else : ?>
 
                                 <div class="white-box overscroll-auto overflow-auto w-full sm:w-<?= $pel['ukuran_kolom']?>">
-                                    <div class="equation-box grid">
+                                    <div class="equation-box flex flex-row">
                                         <div class="flex flex-row">
                                             
 
