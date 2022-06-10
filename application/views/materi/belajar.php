6b2cc99c89f9af9d00baf959eb877958d5f69404
Dida En <firdanb@students.unnes.ac.id>
2022-06-10 12:01:17 +0700

    Coba lagi.

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -83,7 +83,7 @@
 
                                 <div class="white-box overscroll-auto overflow-auto w-full sm:w-<?= $pel['ukuran_kolom']?>">
                                     <div class="equation-box grid overscroll-auto overflow-auto">
-                                        <div class="grid overscroll-auto overflow-auto gap-x-4">
+                                        <div class="flex flex-row overscroll-auto overflow-auto gap-x-4">
                                     
                                             <div class="flex w-10/12 overflow-hidden">
                                                 <?= $pel['isi']; ?>
