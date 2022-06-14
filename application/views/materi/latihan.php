b0d4a576165db9f492783af495a07cf9fbedf167
Dida En <firdanb@students.unnes.ac.id>
2022-06-15 02:26:14 +0700

    Mengembalikan seperti semula karena tanpa iframe pasti mereload halaman.

--- application/views/materi/latihan.php
+++ application/views/materi/latihan.php
@@ -95,8 +95,8 @@
 
                                         <!-- PERTANYAAN -->
                                         <div style="grid-row-start: <?= $latsol['row_start']; ?>; grid-column-start: <?= $latsol['col_start']; ?>; z-index: 10;">
-                                            
-                                            <form action="" method="post" id="form_<?= $box_number; ?>" target="">
+                                            <iframe name="form_<?= $box_number; ?>" style="display: none;"></iframe>
+                                            <form method="post" id="form_<?= $box_number; ?>" target="form_<?= $box_number; ?>">
                                                 <input
                                                 type="text"
                                                 class="pertanyaan <?= $latsol['class']; ?>"
