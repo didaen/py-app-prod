030f3ba7e1af913231d891c299572355011b5ec6
Dida En <firdanb@students.unnes.ac.id>
2022-06-14 19:36:12 +0700

    Menggunakan class pertanyaan-tk1 dan pertanyaan-tk2 pada div kosong supaya class terpanggil.

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -202,6 +202,9 @@
                 <div class="w-1/3"></div>
                 <div class="w-2/3"></div>
             </div>
+
+            <div class="pertanyaan pertanyaan-tk1"></div>
+            <div class="pertanyaan pertanyaan-tk2"></div>
             <!-- AKHIR IV DISPLAY NONE -->
 
         </div>
