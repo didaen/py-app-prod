4660df3fcadc71603209ae58501ac0f95b8639cb
Dida En <firdanb@students.unnes.ac.id>
2022-06-18 17:41:04 +0700

    Menambahkan kondisi pada if supaya fetch tidak dijalankan.

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -485,7 +485,7 @@ showAnswers();
 function sudahDijawab() {
 
 	// Jika ada class pertanyaan yang mana merupakan class yang ada pada card kategori Latihan Soal
-	if(document.getElementsByClassName("pertanyaan") !== null) {
+	if(document.getElementsByClassName("pertanyaan") !== null && document.getElementById('judul') == 'Latihan Soal') {
 
 		// Seleksi semua elemen dengan class pertanyaan
 		let semuaPertanyaan = document.querySelectorAll(".pertanyaan");
