63716ec5b4e390f5b5ef52cd566703c894f76e88
Dida En <firdanb@students.unnes.ac.id>
2022-06-22 06:51:50 +0700

    Sejauh ini sudah ada perkembangan namun malah membuat latar belakangnya menjadi warna merah semua. Semoga bisa menjadi hijau setelah ini. Aamiin.

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,6 +1,7 @@
 // Function yang akan mereload setiap hit BACK dari browser
 
 window.addEventListener( "pageshow", function ( event ) {
+	var perfEntries = performance.getEntriesByType("navigation");
 	var historyTraversal = event.persisted || ( typeof window.performance != "undefined" && perfEntries[0].type === "back_forward" );
 	if ( historyTraversal ) {
 	  // Handle page restore.
