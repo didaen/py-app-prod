fa95ede2453f7600c64d14634d4201caccae21c8
Dida En <firdanb@students.unnes.ac.id>
2022-06-22 09:27:22 +0700

    Menambahakan hidden <option> pada menu dropdown belajar.php dan latihan.php.

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -281,6 +281,7 @@
 
                     <!-- SELECT -->
                     <select class="w-full h-6 px-2 rounded outline-none bg-white" id="card-internal" name="card-number" value="<?= $belajar['card_number']; ?>">
+                        <option value="<?= $belajar['card_number']; ?>" hidden><?= $belajar['card_number']; ?></option>
                         <?php $i = 1; ?>
                         <?php foreach ($info_card as $info) : ?>
                             <option value="<?= $i; ?>"><?= $i; ?>. <?= $info['judul_card']; ?></option>
