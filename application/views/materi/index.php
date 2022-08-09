7d6934cc509f4353a6e7f495cfd815b245713f4e
Dida En <firdanb@students.unnes.ac.id>
2022-08-09 13:54:30 +0700

    Menambahkan tampilan untuk CPL, CPMK, Course Description, dan Subject Matter pada views > materi > index.

--- application/views/materi/index.php
+++ application/views/materi/index.php
@@ -4,6 +4,100 @@
 
     <div class="bg-white text-gray-600 rounded px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
 
+
+        <!-- CPL -->
+        <div class="mb-4">
+
+            <p>
+                Graduate learning outcome (GLO)
+            </p>
+    
+            <div class="white-box overscroll-auto overflow-auto text-sm">
+    
+                <div class="equation-box grid">                         
+                                  
+                    <?php $nomer = 1; ?>
+                    <?php foreach ($cpl as $cpl) : ?>
+                        <div class="flex flex-row mb-4">
+                            <div class="pr-4">
+                                <?= $nomer; ?>.
+                            </div>
+                            <div >
+                                <?= $cpl['cpl']; ?>
+                            </div>
+                        </div>
+                        
+                        <?php $nomer++; ?>
+                    <?php endforeach; ?>
+                    
+                </div>
+                                    
+            </div>
+        </div>
+        <!-- AKHIR CPL -->
+
+
+        <!-- CPMK -->
+        <div class="mb-4">
+
+            <p>
+                Course learning outcome (GLO)
+            </p>
+    
+            <div class="white-box overscroll-auto overflow-auto text-sm">
+    
+                <div class="equation-box grid">                         
+                                  
+                    <?php $nomer = 1; ?>
+                    <?php foreach ($cpmk as $cpmk) : ?>
+                        <div class="flex flex-row mb-4">
+                            <div class="pr-4">
+                                <?= $nomer; ?>.
+                            </div>
+                            <div >
+                                <?= $cpmk['cpmk']; ?>
+                            </div>
+                        </div>
+                        
+                        <?php $nomer++; ?>
+                    <?php endforeach; ?>
+    
+                </div>
+                                    
+            </div>
+        </div>
+        <!-- AKHIR CPMK -->
+
+
+        <!-- COURSE DESCRIPTION -->
+        <div class="mb-4">
+
+            <p>
+                Course Description
+            </p>
+    
+            <div class="white-box overscroll-auto overflow-auto text-sm">
+    
+                <div class="equation-box grid">                         
+                                  
+                    <?php foreach ($course_desc as $cd) : ?>
+                        <div class="flex flex-row mb-4">
+    
+                            <div >
+                                <?= $cd['course_desc']; ?>
+                            </div>
+    
+                        </div>
+                        
+                        <?php $nomer++; ?>
+                    <?php endforeach; ?>
+    
+                </div>
+                                    
+            </div>
+        </div>
+        <!-- AKHIR COURSE DESCRIPTION -->
+
         <form action="<?= base_url(); ?>materi/sub_materi" method="post">
             <label class="block mb-1" for="sub_materi">Pilih Materi : </label>
             <div class="flex flex-col space-y-4">
@@ -18,6 +112,38 @@
 
             </div>
         </form>
+
+
+        <!-- SUBJECT MATTER -->
+        <div class="mb-4">
+
+            <p>
+                Subject Matter
+            </p>
+    
+            <div class="white-box overscroll-auto overflow-auto text-sm">
+    
+                <div class="equation-box grid">                         
+                                  
+                    <?php $nomer = 1; ?>
+                    <?php foreach ($subject_matter as $sm) : ?>
+                        <div class="flex flex-row mb-4">
+                            <div class="pr-4">
+                                <?= $nomer; ?>.
+                            </div>
+                            <div >
+                                <?= $sm['subject_matter']; ?>
+                            </div>
+                        </div>
+                        
+                        <?php $nomer++; ?>
+                    <?php endforeach; ?>
+    
+                </div>
+                                    
+            </div>
+        </div>
+        <!-- AKHIR SUBJECT MATTER -->
 
     </div>
 </div>
