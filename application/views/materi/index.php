fa341e74caeb038658ddad00161e2e2f43cab717
Dida En <firdanb@students.unnes.ac.id>
2022-08-09 15:10:44 +0700

    Mengedit bagian CPMK pada views > materi > index.php.

--- application/views/materi/index.php
+++ application/views/materi/index.php
@@ -5,6 +5,24 @@
     <div class="bg-white text-gray-600 rounded px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
 
 
+        <!-- MATERI -->
+        <form action="<?= base_url(); ?>materi/sub_materi" method="post">
+            <label class="block mb-1" for="sub_materi">Pilih Materi : </label>
+            <div class="flex flex-col space-y-4">
+                <select class="block h-10 px-2 rounded border-2 outline-none bg-white" id="materi" name="materi">
+                    <?php foreach ($materi as $materi) : ?>
+                        <option value="<?= $materi['id']; ?>"><?= $materi['materi']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <div class="flex justify-end">
+                    <button type="submit" name="pilih-materi" class="btn">Pilih</button>
+                </div>
+
+            </div>
+        </form>
+        <!-- AKHIR MATERI -->
+
+
         <!-- CPL -->
         <div class="mb-4">
             <div class="flex flex-row justify-between space-x-4">
@@ -48,12 +66,21 @@
 
         <!-- CPMK -->
         <div class="mb-4">
+            <div class="flex flex-row justify-between space-x-4">
 
-            <p>
-                Course learning outcome (GLO)
-            </p>
+                <div class="mb-4">
+                    Course learning outcome (CLO)
+                </div>
+
+                <div class="flex flex-col justify-center">
+
+                    <input type="checkbox" class="w-5 h-5 absolute z-10 opacity-0" id="cpmk-btn" onchange="klikCpl(this)" autocomplete="off" />
+                    <label for="cpmk-btn" class="text-sm cursor-pointer px-1 bg-blue-500 hover:bg-blue-400 text-white text-center rounded transition duration-300">show</label>
+                </div>
+
+            </div>
     
-            <div class="white-box overscroll-auto overflow-auto text-sm">
+            <div class="white-box overscroll-auto overflow-auto text-sm hidden" id="cpmk">
     
                 <div class="equation-box grid">                         
                                   
@@ -103,20 +130,6 @@
         </div>
         <!-- AKHIR COURSE DESCRIPTION -->
 
-        <form action="<?= base_url(); ?>materi/sub_materi" method="post">
-            <label class="block mb-1" for="sub_materi">Pilih Materi : </label>
-            <div class="flex flex-col space-y-4">
-                <select class="block h-10 px-2 rounded border-2 outline-none bg-white" id="materi" name="materi">
-                    <?php foreach ($materi as $materi) : ?>
-                        <option value="<?= $materi['id']; ?>"><?= $materi['materi']; ?></option>
-                    <?php endforeach; ?>
-                </select>
-                <div class="flex justify-end">
-                    <button type="submit" name="pilih-materi" class="btn">Pilih</button>
-                </div>
-
-            </div>
-        </form>
 
 
         <!-- SUBJECT MATTER -->
