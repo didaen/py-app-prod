f5c622aca374405bba44262246577ce97e8f6fbf
Dida En <firdanb@students.unnes.ac.id>
2022-11-26 00:29:38 +0700

    Mengubah font color pada tentang > index.php dari gray-600 menjadi gray-700.

--- application/views/tentang/index.php
+++ application/views/tentang/index.php
@@ -4,7 +4,7 @@
         <div>
             <img src="<?= base_url('assets/img/pylightgray.png'); ?>" alt="Pengembang" class="w-20">
         </div>
-        <div class="sm:flex flex-row items-center gap-4 text-gray-600 hidden">
+        <div class="sm:flex flex-row items-center gap-4 text-gray-700 hidden">
             <div class="font-medium">
                 <div>Nama</div>
                 <div>Email</div>
@@ -22,7 +22,7 @@
             </div>
         </div>
         <div>
-            <div class="block sm:hidden text-gray-600 text-center">
+            <div class="block sm:hidden text-gray-700 text-center">
                 <div class="font-medium">Nama</div>
                 <div>Firda Nur Baiti</div>
                 <div class="font-medium">Email</div>
@@ -38,7 +38,7 @@
 <div class="mb-10">
 
     <h2 class="bg-gray-300 text-lg font-bold text-blue-1300 text-center py-3">Sekilas tentang pengembang</h2>
-    <div class="bg-white text-gray-600 px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
+    <div class="bg-white text-gray-700 px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
         <p class="text-justify">
             Pengembang adalah mahasiswa akhir Universitas Negeri Semarang yang sedang mengerjakan skripsi. Web ini dibuat oleh pengembang karena terispirasi oleh buku Matematika Teknik oleh K.A Stroud dan web Personal Tutor oleh K.A. Stroud. Pengembang berharap dapat segera menyelesaikan skripsinya dan dapat menambah wawasan pengguna tentang fisika dan matematika.
         </p>
@@ -49,7 +49,7 @@
 <div class="mb-10">
 
     <h2 class="bg-gray-300 text-lg font-bold text-blue-1300 text-center py-3">Referensi</h2>
-    <div class="bg-white text-gray-600 px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
+    <div class="bg-white text-gray-700 px-4 sm:px-8 py-8 border-dashed border-l-8 border-r-8 border-blue-1400">
         <div class="white-box overscroll-auto overflow-auto text-sm">
             <div class="equation-box grid">                         
                               
